82bd14e7893b5c73d8bcb0eb0845e26446757efc
ajcairel <alexanderjcairel@gmail.com>
2022-08-15 17:37:00 -0700

    Registered Photo

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Squad, Squishmallow
+from .models import Squad, Squishmallow, Photo
 
 # Register your models here.
 admin.site.register(Squishmallow)
 admin.site.register(Squad)
+admin.site.register(Photo)
